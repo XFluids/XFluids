a0738691cf2212ff3d88adc086b3223b2dd6ba69
shucheng <shucheng.pan@nwpu.edu.cn>
2022-11-26 16:12:28 +0800

    add BCs for 3D

--- block_sycl.cpp
+++ block_sycl.cpp
@@ -11,10 +11,6 @@ void InitializeFluidStates(sycl::queue &q, array<int, 3> WG, array<int, 3> WI, M
 	// auto local_ndrange = range<3>(WGSize.at(0), WGSize.at(1), WGSize.at(2));	// size of workgroup
 	// auto global_ndrange = range<3>(WISize.at(0), WISize.at(1), WISize.at(2));
 
-    // dim3 dim_grid_max(dim_grid.x + DIM_X, dim_grid.y + DIM_Y, dim_grid.z + DIM_Z);
-    // InitialStatesKernel<<<dim_grid_max, dim_blk>>>(material, U, U1, LU, CnsrvU, CnsrvU1, FluxF, FluxG, FluxH, FluxFw, FluxGw, FluxHw, 
-    //                                                                                 fdata.u, fdata.v, fdata.w, fdata.rho, fdata.p, fdata.H, fdata.c, vof, dx, dy, dz);
-
 	Real *rho = fdata.rho;
 	Real *p = fdata.p;
 	Real *H = fdata.H;
@@ -27,9 +23,9 @@ void InitializeFluidStates(sycl::queue &q, array<int, 3> WG, array<int, 3> WI, M
 		// auto out = sycl::stream(1024, 256, h);
 		h.parallel_for(sycl::nd_range<3>(global_ndrange, local_ndrange), [=](sycl::nd_item<3> index){
     		// 利用get_global_id获得全局指标
-    		int i = index.get_global_id(0) + Bwidth_X - 1;
-    		int j = index.get_global_id(1) + Bwidth_Y;
-			int k = index.get_global_id(2) + Bwidth_Z;
+    		int i = index.get_global_id(0);
+    		int j = index.get_global_id(1);
+			int k = index.get_global_id(2);
 
 			// int ii = index.get_group(0)*index.get_local_range(0) + index.get_local_id(0) + Bwidth_X - 1;
 			// int jj = index.get_group(1)*index.get_local_range(1) + index.get_local_id(1) + Bwidth_Y;
@@ -42,40 +38,231 @@ void InitializeFluidStates(sycl::queue &q, array<int, 3> WG, array<int, 3> WI, M
 	});
 }
 
-void FluidBoundaryCondition(sycl::queue &q, BConditions  *BCs, Real*  d_UI)
+Real GetDt(sycl::queue &q, FlowData &fdata, Real* uvw_c_max, Real const dx, Real const dy, Real const dz)
 {
-	// //x direction
-    // constexpr int dim_block_x = DIM_X ? WarpSize : 1;
-    // constexpr int dim_block_y = DIM_Y ? WarpSize : 1;
-	// constexpr int dim_block_z = 1;//DIM_Z ? WarpSize : 1;
+	Real *rho = fdata.rho;
+	Real *c = fdata.c;
+	Real *u = fdata.u;
+	Real *v = fdata.v;
+	Real *w = fdata.w;
 
+	auto local_ndrange = range<1>(4);	// size of workgroup
+	auto global_ndrange = range<1>(Xmax*Ymax*Zmax);
+    
+	for(int n=0; n<3; n++)
+		uvw_c_max[n] = 0;
+
+    q.submit([&](sycl::handler& h) {
+      	// define reduction objects for sum, min, max reduction
+		// auto reduction_sum = reduction(sum, sycl::plus<>());
+    	auto reduction_max_x = reduction(&(uvw_c_max[0]), sycl::maximum<>());
+		auto reduction_max_y = reduction(&(uvw_c_max[1]), sycl::maximum<>());
+		auto reduction_max_z = reduction(&(uvw_c_max[2]), sycl::maximum<>());
+      
+		h.parallel_for(sycl::nd_range<1>(global_ndrange, local_ndrange), reduction_max_x, reduction_max_y, reduction_max_z, 
+	  	[=](nd_item<1> index, auto& temp_max_x, auto& temp_max_y, auto& temp_max_z)
+		{
+        	auto id = index.get_global_id();
+			// if(id < Xmax*Ymax*Zmax)
+        	temp_max_x.combine(u[id]+c[id]);
+        	temp_max_y.combine(v[id]+c[id]);
+			temp_max_z.combine(w[id]+c[id]);
+      	});
+    }).wait();
+
+	Real dtref = 0.0;
+	#if DIM_X
+	dtref += uvw_c_max[0]/dx;
+	#endif
+	#if DIM_Y
+	dtref += uvw_c_max[1]/dy;
+	#endif
+	#if DIM_Z
+	dtref += uvw_c_max[2]/dz;
+	#endif
+	return CFLnumber/dtref;
+}
+
+void UpdateFluidStateFlux(sycl::queue &q, Real*  UI, FlowData &fdata, Real*  FluxF, Real*  FluxG, Real*  FluxH, Real const Gamma)
+{
+	auto local_ndrange = range<3>(dim_block_x, dim_block_y, dim_block_z);	// size of workgroup
+	auto global_ndrange = range<3>(Xmax, Ymax, Zmax);
+
+	Real *rho = fdata.rho;
+	Real *p = fdata.p;
+	Real *H = fdata.H;
+	Real *c = fdata.c;
+	Real *u = fdata.u;
+	Real *v = fdata.v;
+	Real *w = fdata.w;
+
+	q.submit([&](sycl::handler &h){
+		h.parallel_for(sycl::nd_range<3>(global_ndrange, local_ndrange), [=](sycl::nd_item<3> index){
+    		int i = index.get_global_id(0);
+    		int j = index.get_global_id(1);
+			int k = index.get_global_id(2);
+
+			UpdateFuidStatesKernel(i, j, k, UI, FluxF, FluxG, FluxH, rho, p, c, H, u, v, w, Gamma);
+		});
+	});
+
+	q.wait();
+}
+
+void UpdateURK3rd(sycl::queue &q, Real* U, Real* U1, Real* LU, Real const dt, int flag)
+{
+	auto local_ndrange = range<3>(dim_block_x, dim_block_y, dim_block_z);	// size of workgroup
+	auto global_ndrange = range<3>(X_inner, Y_inner, Z_inner);
+
+	q.submit([&](sycl::handler &h){
+		h.parallel_for(sycl::nd_range<3>(global_ndrange, local_ndrange), [=](sycl::nd_item<3> index){
+    		int i = index.get_global_id(0) + Bwidth_X;
+    		int j = index.get_global_id(1) + Bwidth_Y;
+			int k = index.get_global_id(2) + Bwidth_Z;
+
+			UpdateURK3rdKernel(i, j, k, U, U1, LU, dt, flag);
+		});
+	});
+
+	q.wait();
+}
+
+void GetLU(sycl::queue &q, Real* UI, Real* LU, Real* FluxF, Real* FluxG, Real* FluxH, 
+            Real* FluxFw, Real* FluxGw, Real* FluxHw, Real const Gamma, int const Mtrl_ind, 
+            FlowData &fdata, Real* eigen_local, Real const dx, Real const dy, Real const dz)
+{
+	Real *rho = fdata.rho;
+	Real *p = fdata.p;
+	Real *H = fdata.H;
+	Real *c = fdata.c;
+	Real *u = fdata.u;
+	Real *v = fdata.v;
+	Real *w = fdata.w;
+
+    bool is_3d = DIM_X*DIM_Y*DIM_Z ? true : false;
+
+	auto local_ndrange = range<3>(dim_block_x, dim_block_y, dim_block_z);
+	auto global_ndrange_max = range<3>(Xmax, Ymax, Zmax);
+	auto global_ndrange_inner = range<3>(X_inner, Y_inner, Z_inner);
+
+	#if DIM_X
+	//proceed at x directiom and get F-flux terms at node wall
+  	auto global_ndrange_x = range<3>(X_inner+local_ndrange[0], Y_inner, Z_inner);
+	
+	auto e = q.submit([&](sycl::handler &h){
+		h.parallel_for(sycl::nd_range<3>(global_ndrange_max, local_ndrange), [=](sycl::nd_item<3> index){
+    		int i = index.get_global_id(0);
+    		int j = index.get_global_id(1);
+			int k = index.get_global_id(2);
+			GetLocalEigen(i, j, k, 1.0, 0.0, 0.0, eigen_local, u, v, w, c);
+		});
+	});
+
+	q.submit([&](sycl::handler &h){
+		h.depends_on(e);
+		h.parallel_for(sycl::nd_range<3>(global_ndrange_x, local_ndrange), [=](sycl::nd_item<3> index){
+    		int i = index.get_global_id(0) + Bwidth_X - 1;
+    		int j = index.get_global_id(1) + Bwidth_Y;
+			int k = index.get_global_id(2) + Bwidth_Z;
+			ReconstructFluxX(i, j, k, UI, FluxF, FluxFw, eigen_local, rho, u, v, w, H, dx);
+		});
+	}).wait();
+	#endif
+
+	// #if DIM_Y
+	// //proceed at y directiom and get G-flux terms at node wall
+    // GetLocalEigen<<<dim_grid_max, dim_blk>>>(0.0, 1.0, 0.0, eigen_local, fdata.u, fdata.v, fdata.w, fdata.c);
+    // CheckCUDAErrors(cudaGetLastError());
+    // // dim3 dim_grid_y(dim_grid.x, dim_grid.y + 1, dim_grid.z);
+    // // ReconstructFluxY<<<dim_grid_y, dim_blk>>>(UI, FluxG, FluxGw, eigen_local, fdata.rho, fdata.u, fdata.v, fdata.w, fdata.H, dy);
+    // blocksize = is_3d ?  dim3(WarpSize,WarpSize/2, 1)  : dim_blk;
+    // gridsize = is_3d ? dim3(X_inner/blocksize.x, Y_inner/blocksize.y+1, Z_inner/blocksize.z) : dim3(dim_grid.x, dim_grid.y + 1, dim_grid.z);
+    // ReconstructFluxY<<<gridsize, blocksize>>>(UI, FluxG, FluxGw, phi, tag_bnd_xt, Rgn_ind, Gamma, Mtrl_ind, eigen_local, fdata.rho, fdata.u, fdata.v, fdata.w, fdata.H, fdata.c, dy);
+	// #endif
+
+	// #if DIM_Z
+	// //proceed at y directiom and get G-flux terms at node wall
+    // GetLocalEigen<<<dim_grid_max, dim_blk>>>(0.0, 0.0, 1.0, eigen_local, fdata.u, fdata.v, fdata.w, fdata.c);
+    // CheckCUDAErrors(cudaGetLastError());
+    // // dim3 dim_grid_z(dim_grid.x, dim_grid.y, dim_grid.z + 1);
+    // // ReconstructFluxZ<<<dim_grid_z, dim_blk>>>(UI, FluxH, FluxHw, eigen_local, fdata.rho, fdata.u, fdata.v, fdata.w, fdata.H, dz);
+    // blocksize = is_3d ?  dim3(WarpSize/2, 1, WarpSize)  : dim_blk;
+    // gridsize = is_3d ? dim3(X_inner/blocksize.x, Y_inner/blocksize.y, Z_inner/blocksize.z+1) : dim3(dim_grid.x, dim_grid.y, dim_grid.z + 1);
+    // ReconstructFluxZ<<<gridsize, blocksize>>>(UI, FluxH, FluxHw, phi, tag_bnd_xt, Rgn_ind, Gamma, eigen_local, fdata.rho, fdata.u, fdata.v, fdata.w, fdata.H, dz);
+	// #endif
+
+	//update LU from cell-face fluxes
+    #if NumFluid == 2
+    // UpdateFluidMPLU<<<dim_grid, dim_blk>>>(LU, FluxFw, FluxGw, FluxHw, phi, tag_bnd_hlf, Rgn_ind, x_swth, y_swth, z_swth, exchange, dx, dy, dz);
+    #else
+	q.submit([&](sycl::handler &h){
+		h.parallel_for(sycl::nd_range<3>(global_ndrange_inner, local_ndrange), [=](sycl::nd_item<3> index){
+    		int i = index.get_global_id(0) + Bwidth_X;
+    		int j = index.get_global_id(1) + Bwidth_Y;
+			int k = index.get_global_id(2) + Bwidth_Z;
+
+			UpdateFluidLU(i, j, k, LU, FluxFw, FluxGw, FluxHw, dx, dy, dz);
+		});
+	}).wait();
+    #endif
+}
+
+void FluidBoundaryCondition(sycl::queue &q, BConditions BCs[6], Real*  d_UI)
+{
     #if DIM_X
 	auto local_ndrange_x = range<3>(Bwidth_X, dim_block_y, dim_block_z);	// size of workgroup
 	auto global_ndrange_x = range<3>(Bwidth_X, Ymax, Zmax);
 
+	BConditions BC0 = BCs[0], BC1 = BCs[1];
+
 	q.submit([&](sycl::handler &h){
 		h.parallel_for(sycl::nd_range<3>(global_ndrange_x, local_ndrange_x), [=](sycl::nd_item<3> index){
-    		int i = index.get_global_id(0) + Bwidth_X - 1;
-    		int j = index.get_global_id(1) + Bwidth_Y;
-			int k = index.get_global_id(2) + Bwidth_Z;
+    		int i0 = index.get_global_id(0) + 0;
+			int i1 = index.get_global_id(0) + Xmax-Bwidth_X;
+    		int j = index.get_global_id(1);
+			int k = index.get_global_id(2);
 
-			FluidBCKernelX(i, j, k, &(BCs[0]), d_UI, 0, 0, Bwidth_X, 1);
-			FluidBCKernelX(i, j, k, &(BCs[1]), d_UI, Xmax-Bwidth_X, X_inner, Xmax-Bwidth_X-1, -1);
+			FluidBCKernelX(i0, j, k, BC0, d_UI, 0, Bwidth_X, 1);
+			FluidBCKernelX(i1, j, k, BC1, d_UI, X_inner, Xmax-Bwidth_X-1, -1);
 		});
 	});
     #endif
 
-    // #if DIM_Y
-	// dim3 dim_blk_y(dim_block_x, Bwidth_Y, dim_block_z);
-    // dim3 dim_grid_y(X_inner/dim_block_x + DIM_X, 1, Z_inner/dim_block_z + DIM_Z);
-    // FluidBCKernelY<<<dim_grid_y, dim_blk_y>>>(BCs[2], d_UI, 0, 0, Bwidth_Y, 1);
-    // FluidBCKernelY<<<dim_grid_y, dim_blk_y>>>(BCs[3], d_UI, Ymax-Bwidth_Y, Y_inner, Ymax-Bwidth_Y-1, -1);
-    // #endif
+    #if DIM_Y
+	auto local_ndrange_y = range<3>(dim_block_x, Bwidth_Y, dim_block_z);	// size of workgroup
+	auto global_ndrange_y = range<3>(Xmax, Bwidth_Y, Zmax);
 
-    // #if DIM_Z
-	// dim3 dim_blk_z(1, dim_block_y, Bwidth_Z);
-    // dim3 dim_grid_z(X_inner/1 + DIM_X, Y_inner/dim_block_y + DIM_Y, 1);
-    // FluidBCKernelZ<<<dim_grid_z, dim_blk_z>>>(BCs[4], d_UI, 0, 0, Bwidth_Z, 1);
-    // FluidBCKernelZ<<<dim_grid_z, dim_blk_z>>>(BCs[5], d_UI, Zmax-Bwidth_Z, Z_inner, Zmax-Bwidth_Z-1, -1);
-    // #endif
+	BConditions BC2 = BCs[2], BC3 = BCs[3];
+
+	q.submit([&](sycl::handler &h){
+		h.parallel_for(sycl::nd_range<3>(global_ndrange_y, local_ndrange_y), [=](sycl::nd_item<3> index){
+    		int i = index.get_global_id(0);
+			int j0 = index.get_global_id(1) + 0;
+    		int j1 = index.get_global_id(1) + Ymax-Bwidth_Y;
+			int k = index.get_global_id(2);
+
+    		FluidBCKernelY(i, j0, k, BC2, d_UI, 0, Bwidth_Y, 1);
+    		FluidBCKernelY(i, j1, k, BC3, d_UI, Y_inner, Ymax-Bwidth_Y-1, -1);
+		});
+	});
+    #endif
+
+    #if DIM_Z
+	auto local_ndrange_z = range<3>(dim_block_x, dim_block_y, Bwidth_Z);	// size of workgroup
+	auto global_ndrange_z = range<3>(Xmax, Ymax, Bwidth_Z);
+
+	BConditions BC4 = BCs[4], BC5 = BCs[5];
+
+	q.submit([&](sycl::handler &h){
+		h.parallel_for(sycl::nd_range<3>(global_ndrange_z, local_ndrange_z), [=](sycl::nd_item<3> index){
+    		int i = index.get_global_id(0);
+			int j = index.get_global_id(1);
+    		int k0 = index.get_global_id(2) + 0;
+			int k1 = index.get_global_id(2) + Zmax-Bwidth_Z;
+
+    		FluidBCKernelZ(i, j, k0, BC4, d_UI, 0, Bwidth_Z, 1);
+    		FluidBCKernelZ(i, j, k1, BC5, d_UI, Z_inner, Zmax-Bwidth_Z-1, -1);
+		});
+	});
+    #endif
 }
--- sycl_kernels.cpp
+++ sycl_kernels.cpp
@@ -11,18 +11,18 @@ void InitialStatesKernel(int i, int j, int k, MaterialProperty* material, Real* 
 {
     int id = Xmax*Ymax*k + Xmax*j + i;
 
-    #if DIM_X
-    if(i >= Xmax)
-        return;
-    #endif
-    #if DIM_Y
-    if(j >= Ymax)
-        return;
-    #endif
-    #if DIM_Z
-    if(k >= Zmax)
-        return;
-    #endif
+    // #if DIM_X
+    // if(i >= Xmax)
+    //     return;
+    // #endif
+    // #if DIM_Y
+    // if(j >= Ymax)
+    //     return;
+    // #endif
+    // #if DIM_Z
+    // if(k >= Zmax)
+    //     return;
+    // #endif
 
 	#if USE_DP
 	Real one_float = 1.0;
@@ -146,13 +146,8 @@ void InitialStatesKernel(int i, int j, int k, MaterialProperty* material, Real* 
 // add "sycl::nd_item<3> item" for get_global_id
 // add "stream const s" for output
 extern SYCL_EXTERNAL void ReconstructFluxX(int i, int j, int k, Real* UI, Real* Fx, Real* Fxwall, Real* eigen_local, Real* rho, Real* u, Real* v, 
-                                            Real* w, Real* H, Real dx, Real dy)
+                                            Real* w, Real* H, Real dx)
 {
-    // // 利用get_global_id获得全局指标
-    // int i = item.get_global_id(0) + Bwidth_X - 1;
-    // int j = item.get_global_id(1) + Bwidth_Y;
-	// int k = item.get_global_id(2) + Bwidth_Z;
-
     int id_l = Xmax*Ymax*k + Xmax*j + i;
     int id_r = Xmax*Ymax*k + Xmax*j + i + 1;
 
@@ -162,83 +157,146 @@ extern SYCL_EXTERNAL void ReconstructFluxX(int i, int j, int k, Real* UI, Real* 
     if(i>= X_inner+Bwidth_X)
         return;
 
-    // Real eigen_l[Emax][Emax], eigen_r[Emax][Emax];
+    Real eigen_l[Emax][Emax], eigen_r[Emax][Emax];
 
-	// //preparing some interval value for roe average
-	// Real D	=	sqrt(rho[id_r]/rho[id_l]);
-	// #if USE_DP
-	// Real D1	=	1.0 / (D + 1.0);
-	// #else
-	// Real D1	=	1.0f / (D + 1.0f);
-	// #endif
-	// Real _u	=	(u[id_l] + D*u[id_r])*D1;
-	// Real _v	=	(v[id_l] + D*v[id_r])*D1;
-	// Real _w	=	(w[id_l] + D*w[id_r])*D1;
-	// Real _H	=	(H[id_l] + D*H[id_r])*D1;
-	// Real _rho = sqrt(rho[id_r]*rho[id_l]);
+	//preparing some interval value for roe average
+	Real D	=	sqrt(rho[id_r]/rho[id_l]);
+	#if USE_DP
+	Real D1	=	1.0 / (D + 1.0);
+	#else
+	Real D1	=	1.0f / (D + 1.0f);
+	#endif
+	Real _u	=	(u[id_l] + D*u[id_r])*D1;
+	Real _v	=	(v[id_l] + D*v[id_r])*D1;
+	Real _w	=	(w[id_l] + D*w[id_r])*D1;
+	Real _H	=	(H[id_l] + D*H[id_r])*D1;
+	Real _rho = sqrt(rho[id_r]*rho[id_l]);
 
-    // RoeAverage_x(eigen_l, eigen_r, _rho, _u, _v, _w, _H, D, D1);
+    RoeAverage_x(eigen_l, eigen_r, _rho, _u, _v, _w, _H, D, D1);
 
-    // Real uf[10],  ff[10], pp[10], mm[10];
-    // Real f_flux, _p[Emax][Emax];
+    Real uf[10],  ff[10], pp[10], mm[10];
+    Real f_flux, _p[Emax][Emax];
 
-	// // construct the right value & the left value scalar equations by characteristic reduction			
-	// // at i+1/2 in x direction
-    // // #pragma unroll Emax
-	// for(int n=0; n<Emax; n++){
-    //     #if USE_DP
-    //     Real eigen_local_max = 0.0;
-    //     #else
-    //     Real eigen_local_max = 0.0f;
-    //     #endif
-    //     for(int m=-2; m<=3; m++){
-    //         int id_local = Xmax*Ymax*k + Xmax*j + i + m;
-    //         eigen_local_max = sycl::max(eigen_local_max, fabs(eigen_local[Emax*id_local+n]));//local lax-friedrichs	
-    //     }
+	// construct the right value & the left value scalar equations by characteristic reduction			
+	// at i+1/2 in x direction
+    // #pragma unroll Emax
+	for(int n=0; n<Emax; n++){
+        #if USE_DP
+        Real eigen_local_max = 0.0;
+        #else
+        Real eigen_local_max = 0.0f;
+        #endif
+        for(int m=-2; m<=3; m++){
+            int id_local = Xmax*Ymax*k + Xmax*j + i + m;
+            eigen_local_max = sycl::max(eigen_local_max, fabs(eigen_local[Emax*id_local+n]));//local lax-friedrichs	
+        }
 
-	// 	for(int m=i-3; m<=i+4; m++){	// 3rd oder and can be modified
-    //         int id_local = Xmax*Ymax*k + Xmax*j + m;
-    //         #if USE_DP
-	// 		uf[m-i+3] = 0.0;
-	// 		ff[m-i+3] = 0.0;
-    //         #else
-	// 		uf[m-i+3] = 0.0f;
-	// 		ff[m-i+3] = 0.0f;
-    //         #endif
+		for(int m=i-3; m<=i+4; m++){	// 3rd oder and can be modified
+            int id_local = Xmax*Ymax*k + Xmax*j + m;
+            #if USE_DP
+			uf[m-i+3] = 0.0;
+			ff[m-i+3] = 0.0;
+            #else
+			uf[m-i+3] = 0.0f;
+			ff[m-i+3] = 0.0f;
+            #endif
 
-	// 		for(int n1=0; n1<Emax; n1++){
-	// 			uf[m-i+3] = uf[m-i+3] + UI[Emax*id_local+n1]*eigen_l[n][n1];
-	// 			ff[m-i+3] = ff[m-i+3] + Fx[Emax*id_local+n1]*eigen_l[n][n1];
-	// 		}
-	// 		// for local speed
-	// 		pp[m-i+3] = 0.5f*(ff[m-i+3] + eigen_local_max*uf[m-i+3]);
-	// 		mm[m-i+3] = 0.5f*(ff[m-i+3] - eigen_local_max*uf[m-i+3]);
-    //     }
+			for(int n1=0; n1<Emax; n1++){
+				uf[m-i+3] = uf[m-i+3] + UI[Emax*id_local+n1]*eigen_l[n][n1];
+				ff[m-i+3] = ff[m-i+3] + Fx[Emax*id_local+n1]*eigen_l[n][n1];
+			}
+			// for local speed
+			pp[m-i+3] = 0.5f*(ff[m-i+3] + eigen_local_max*uf[m-i+3]);
+			mm[m-i+3] = 0.5f*(ff[m-i+3] - eigen_local_max*uf[m-i+3]);
+        }
 
-	// 	// calculate the scalar numerical flux at x direction
-    //     #if USE_DP
-    //     f_flux = (weno5old_P(&pp[3], dx) + weno5old_M(&mm[3], dx))/6.0;
-    //     #else
-    //     f_flux = (weno5old_P(&pp[3], dx) + weno5old_M(&mm[3], dx))/6.0f;
-    //     #endif
+		// calculate the scalar numerical flux at x direction
+        #if USE_DP
+        f_flux = (weno5old_P(&pp[3], dx) + weno5old_M(&mm[3], dx))/6.0;
+        #else
+        f_flux = (weno5old_P(&pp[3], dx) + weno5old_M(&mm[3], dx))/6.0f;
+        #endif
 
-	// 	// get Fp
-	// 	for(int n1=0; n1<Emax; n1++)
-	// 		_p[n][n1] = f_flux*eigen_r[n1][n];
-    // }
+		// get Fp
+		for(int n1=0; n1<Emax; n1++)
+			_p[n][n1] = f_flux*eigen_r[n1][n];
+    }
 
-	// // reconstruction the F-flux terms
-	// for(int n=0; n<Emax; n++){
-    //     #if USE_DP
-    //     Real fluxx = 0.0;
-    //     #else
-    //     Real fluxx = 0.0f;
-    //     #endif
-	// 	for(int n1=0; n1<Emax; n1++) {
-    //         fluxx += _p[n1][n];
-	// 	}
-    //     Fxwall[Emax*id_l+n] = fluxx;
-	// }
+	// reconstruction the F-flux terms
+	for(int n=0; n<Emax; n++){
+        #if USE_DP
+        Real fluxx = 0.0;
+        #else
+        Real fluxx = 0.0f;
+        #endif
+		for(int n1=0; n1<Emax; n1++) {
+            fluxx += _p[n1][n];
+		}
+        Fxwall[Emax*id_l+n] = fluxx;
+	}
+}
+
+void GetLocalEigen(int i, int j, int k, Real AA, Real BB, Real CC, Real* eigen_local, Real* u, Real* v, Real* w, Real* c)
+{
+    int id = Xmax*Ymax*k + Xmax*j + i;
+
+    #if DIM_X
+    if(i >= Xmax)
+    return;
+    #endif
+    #if DIM_Y
+    if(j >= Ymax)
+    return;
+    #endif
+    #if DIM_Z
+    if(k >= Zmax)
+    return;
+    #endif
+
+    Real	uu = AA*u[id] + BB*v[id] + CC*w[id];
+    Real	uuPc = uu + c[id];
+    Real	uuMc = uu - c[id];
+    //local eigen values
+    eigen_local[Emax*id+0] = uuMc;
+    eigen_local[Emax*id+1] = uu;
+    eigen_local[Emax*id+2] = uu;
+    eigen_local[Emax*id+3] = uu;
+    eigen_local[Emax*id+4] = uuPc;
+}
+
+void UpdateFluidLU(int i, int j, int k, Real* LU, Real* FluxFw, Real* FluxGw, Real* FluxHw, 
+                    Real const dx, Real const dy, Real const dz)
+{
+    int id = Xmax*Ymax*k + Xmax*j + i;
+
+    #if DIM_X
+    int id_im = Xmax*Ymax*k + Xmax*j + i - 1;
+    #endif
+    #if DIM_Y
+    int id_jm = Xmax*Ymax*k + Xmax*(j-1) + i;
+    #endif
+    #if DIM_Z
+    int id_km = Xmax*Ymax*(k-1) + Xmax*j + i;
+    #endif
+
+    for(int n=0; n<Emax; n++){
+        #if USE_DP
+        Real LU0 = 0.0;
+        #else
+        Real LU0 = 0.0f;
+        #endif
+        
+        #if DIM_X
+        LU0 += (FluxFw[Emax*id_im+n] - FluxFw[Emax*id+n])/dx;
+        #endif
+        #if DIM_Y
+        LU0 += (FluxGw[Emax*id_jm+n] - FluxGw[Emax*id+n])/dy;
+        #endif
+        #if DIM_Z
+        LU0 += (FluxHw[Emax*id_km+n] - FluxHw[Emax*id+n])/dz;
+        #endif
+        LU[Emax*id+n] = LU0;
+    }
 }
 
 extern SYCL_EXTERNAL void testkernel(int i, int j, int k, Real* UI, Real* Fx, Real* Fxwall, Real* eigen_local, Real*  u, Real*  v, Real*  w, Real*  rho,
@@ -253,4 +311,224 @@ extern SYCL_EXTERNAL void testkernel(int i, int j, int k, Real* UI, Real* Fx, Re
     if(i>= X_inner+Bwidth_X)
         return;
 
+}
+
+void UpdateFuidStatesKernel(int i, int j, int k, Real*  UI, Real*  FluxF, Real*  FluxG, Real*  FluxH, 
+                                                            Real*  rho, Real*  p, Real*  c, Real*  H, Real*  u, Real*  v, Real*  w, Real const Gamma)
+{
+    int id = Xmax*Ymax*k + Xmax*j + i;
+
+    #if DIM_X
+    if(i >= Xmax)
+        return;
+    #endif
+    #if DIM_Y
+    if(j >= Ymax)
+        return;
+    #endif
+    #if DIM_Z
+    if(k >= Zmax)
+        return;
+    #endif
+
+    Real U[Emax] = {UI[Emax*id+0], UI[Emax*id+1], UI[Emax*id+2], UI[Emax*id+3], UI[Emax*id+4]};
+
+    GetStates(U, rho[id], u[id], v[id], w[id], p[id], H[id], c[id], Gamma);
+
+    Real *Fx = &(FluxF[Emax*id]);
+    Real *Fy = &(FluxG[Emax*id]);
+    Real *Fz = &(FluxH[Emax*id]);
+    GetPhysFlux(U, Fx, Fy, Fz, rho[id], u[id], v[id], w[id], p[id], H[id], c[id]);
+}
+
+void UpdateURK3rdKernel(int i, int j, int k, Real* U, Real* U1, Real* LU, Real const dt, int flag)
+{
+    int id = Xmax*Ymax*k + Xmax*j + i;
+
+    switch(flag) {
+        case 1:
+        for(int n=0; n<Emax; n++)
+            U1[Emax*id+n] = U[Emax*id+n] + dt*LU[Emax*id+n];
+        break;
+        case 2:
+        for(int n=0; n<Emax; n++){
+            #if USE_DP
+            U1[Emax*id+n] = 0.75*U[Emax*id+n] + 0.25*U1[Emax*id+n] + 0.25*dt*LU[Emax*id+n];
+            #else
+            U1[Emax*id+n] = 0.75f*U[Emax*id+n] + 0.25f*U1[Emax*id+n] + 0.25f*dt*LU[Emax*id+n];
+            #endif
+        }   
+        break;
+        case 3:
+        for(int n=0; n<Emax; n++){
+            #if USE_DP
+            U[Emax*id+n] = (U[Emax*id+n] + 2.0*U1[Emax*id+n])/3.0 + 2.0*dt*LU[Emax*id+n]/3.0;
+            #else
+            U[Emax*id+n] = (U[Emax*id+n] + 2.0f*U1[Emax*id+n])/3.0f + 2.0f*dt*LU[Emax*id+n]/3.0f;
+            #endif
+        }
+        break;
+    }
+}
+
+void FluidBCKernelX(int i, int j, int k, BConditions  const BC, Real* d_UI, int const mirror_offset, int const index_inner, int const sign)
+{
+    int id = Xmax*Ymax*k + Xmax*j + i;
+
+    #if DIM_Y
+    if(j >= Ymax)
+        return;
+    #endif
+    #if DIM_Z
+    if(k >= Zmax)
+        return;
+    #endif
+
+    switch(BC) {
+        case Symmetry:
+        {
+            int offset = 2*(Bwidth_X+mirror_offset)-1;
+            int target_id = Xmax*Ymax*k + Xmax*j + (offset-i);
+            for(int n=0; n<Emax; n++)	d_UI[Emax*id+n] = d_UI[Emax*target_id+n];
+            d_UI[Emax*id+1] = -d_UI[Emax*target_id+1];
+        }
+        break;
+
+        case Periodic:
+        {
+            int target_id = Xmax*Ymax*k + Xmax*j + (i + sign*X_inner);
+            for(int n=0; n<Emax; n++)	d_UI[Emax*id+n] = d_UI[Emax*target_id+n];
+        }
+        break;
+
+        case Inflow:
+        break;
+
+        case Outflow:
+        {
+            int target_id = Xmax*Ymax*k + Xmax*j + index_inner;
+            for(int n=0; n<Emax; n++)	d_UI[Emax*id+n] = d_UI[Emax*target_id+n];
+        }
+        break;
+
+        case Wall:
+        {
+            int offset = 2*(Bwidth_X+mirror_offset)-1;
+            int target_id = Xmax*Ymax*k + Xmax*j + (offset-i);
+            d_UI[Emax*id+0] = d_UI[Emax*target_id+0];
+            d_UI[Emax*id+1] = -d_UI[Emax*target_id+1];
+            d_UI[Emax*id+2] = -d_UI[Emax*target_id+2];
+            d_UI[Emax*id+3] = -d_UI[Emax*target_id+3];
+            d_UI[Emax*id+4] = d_UI[Emax*target_id+4];
+        }
+        break;
+    }
+}
+
+void FluidBCKernelY(int i, int j, int k, BConditions const BC, Real* d_UI, int const mirror_offset, int const index_inner, int const sign)
+{
+    int id = Xmax*Ymax*k + Xmax*j + i;
+
+    #if DIM_X
+    if(i >= Xmax)
+        return;
+    #endif
+    #if DIM_Z
+    if(k >= Zmax)
+        return;
+    #endif
+
+    switch(BC) {
+        case Symmetry:
+        {
+            int offset = 2*(Bwidth_Y+mirror_offset)-1;
+            int target_id = Xmax*Ymax*k + Xmax*(offset-j) + i;
+            for(int n=0; n<Emax; n++)	d_UI[Emax*id+n] = d_UI[Emax*target_id+n];
+            d_UI[Emax*id+2] = -d_UI[Emax*target_id+2];
+        }
+        break;
+
+        case Periodic:
+        {
+            int target_id = Xmax*Ymax*k + Xmax*(j + sign*Y_inner) + i;
+            for(int n=0; n<Emax; n++)	d_UI[Emax*id+n] = d_UI[Emax*target_id+n];
+        }
+        break;
+
+        case Inflow:
+        break;
+
+        case Outflow:
+        {
+            int target_id = Xmax*Ymax*k + Xmax*index_inner + i;
+            for(int n=0; n<Emax; n++)	d_UI[Emax*id+n] = d_UI[Emax*target_id+n];
+        }
+        break;
+
+        case Wall:
+        {
+            int offset = 2*(Bwidth_Y+mirror_offset)-1;
+            int target_id = Xmax*Ymax*k + Xmax*(offset-j) + i;
+            d_UI[Emax*id+0] = d_UI[Emax*target_id+0];
+            d_UI[Emax*id+1] = -d_UI[Emax*target_id+1];
+            d_UI[Emax*id+2] = -d_UI[Emax*target_id+2];
+            d_UI[Emax*id+3] = -d_UI[Emax*target_id+3];
+            d_UI[Emax*id+4] = d_UI[Emax*target_id+4];
+        }
+        break;
+    }
+}
+
+void FluidBCKernelZ(int i, int j, int k, BConditions const BC, Real* d_UI, int const  mirror_offset, int const index_inner, int const sign)
+{
+    int id = Xmax*Ymax*k + Xmax*j + i;
+
+    #if DIM_X
+    if(i >= Xmax)
+        return;
+    #endif
+    #if DIM_Y
+    if(j >= Ymax)
+        return;
+    #endif
+
+    switch(BC) {
+        case Symmetry:
+        {
+            int offset = 2*(Bwidth_Z+mirror_offset)-1;
+            int target_id = Xmax*Ymax*(offset-k) + Xmax*j + i;
+            for(int n=0; n<Emax; n++)	d_UI[Emax*id+n] = d_UI[Emax*target_id+n];
+            d_UI[Emax*id+3] = -d_UI[Emax*target_id+3];
+        }
+        break;
+
+        case Periodic:
+        {
+            int target_id = Xmax*Ymax*(k + sign*Z_inner) + Xmax*j + i;
+            for(int n=0; n<Emax; n++)	d_UI[Emax*id+n] = d_UI[Emax*target_id+n];
+        }
+        break;
+
+        case Inflow:
+        break;
+
+        case Outflow:
+        {
+            int target_id = Xmax*Ymax*index_inner + Xmax*j + i;
+            for(int n=0; n<Emax; n++)	d_UI[Emax*id+n] = d_UI[Emax*target_id+n];
+        }
+        break;
+
+        case Wall:
+        {
+            int offset = 2*(Bwidth_Z+mirror_offset)-1;
+            int target_id = Xmax*Ymax*(k-offset) + Xmax*j + i;
+            d_UI[Emax*id+0] = d_UI[Emax*target_id+0];
+            d_UI[Emax*id+1] = -d_UI[Emax*target_id+1];
+            d_UI[Emax*id+2] = -d_UI[Emax*target_id+2];
+            d_UI[Emax*id+3] = -d_UI[Emax*target_id+3];
+            d_UI[Emax*id+4] = d_UI[Emax*target_id+4];
+        }
+        break;
+    }
 }
