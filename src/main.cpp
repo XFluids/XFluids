b760b022624ff7ef1b50b0fe7894aedb5ff20170
JinlongLi <ljl66623@mail.nwpu.edu.cn>
2023-03-03 20:18:55 +0800

    a bug fixed for acceletor select

--- src/main.cpp
+++ src/main.cpp
@@ -48,9 +48,10 @@ int main(int argc, char *argv[])
 	int nRanks = 1;
 #endif // USE_MPI
 	ConfigMap configMap = broadcast_parameters(std::string(argv[1]));
+	// num_GPUS:number of GPU on this cluster, Pform_id: the first GPU's ID in all accelerators sycl detected
+	int device_id = rank % num_GPUs + Pform_id;
 	// accelerator_selector device;
-	int device_id = rank % num_GPUs; // num_GPUS = 2 means two GPUs on this cluster
-	auto device = sycl::platform::get_platforms()[Pform_id].get_devices()[0];
+	auto device = sycl::platform::get_platforms()[device_id].get_devices()[0];
 	sycl::queue q(device);
 	Setup setup(configMap, q);
 	// Create MPI session if MPI enabled
